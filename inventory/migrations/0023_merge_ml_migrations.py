7df0cdb8d9f4dc9132aa4cc906a1f38e14271bd6
Alejo <alejosalmerong@gmail.com>
2026-01-05 00:21:56 +0000

    Merge ML migrations

---
+++ inventory/migrations/0023_merge_ml_migrations.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("inventory", "0021_rename_ml_notification_user_id"),
+        ("inventory", "0022_ml_item_sales_fields"),
+    ]
+
+    operations = []
